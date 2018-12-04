bdb6a4e38f3f771621ff7d475d6c279d10b98f5e
AARaven <voronin.arty@gmail.com>
2018-12-04 16:12:31 +0400

    pre alfa +generators

--- src/test/java/SeleniumTest/WebDriverConfig.java
+++ src/test/java/SeleniumTest/WebDriverConfig.java
@@ -1,5 +1,6 @@
 package SeleniumTest;
 
+import net.bytebuddy.utility.RandomString;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -10,6 +11,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 public class WebDriverConfig {
@@ -17,6 +19,31 @@ public class WebDriverConfig {
     private WebDriver driver;
     private String startPage;
     private String registrationTitle;
+
+    private String accountEmailGenerator() {
+        RandomString randomFirstPice = new RandomString(5);
+        RandomString randomSecondPice = new RandomString(5);
+        RandomString randomDomen = new RandomString(3);
+        return String.format("%s" + "@" + "%s" + "." + "%s",
+                randomFirstPice.nextString(),
+                randomSecondPice.nextString(),
+                randomDomen.nextString());
+    }
+
+    private String passwordGenerator() {
+        RandomString passwd = new RandomString(10);
+        return passwd.nextString();
+    }
+
+    private String stringDataGenerator() {
+        RandomString randomData = new RandomString(10);
+        return randomData.nextString().replaceAll("\\d*","");
+    }
+
+    private String numberDataGenerator() {
+        Random randomNumberData = new Random();
+        return String.valueOf(randomNumberData.nextInt(1000000));
+    }
 
     @BeforeClass
     public void startDriver() {
@@ -34,106 +61,106 @@ public class WebDriverConfig {
     public void quitDriver() {
         driver.close();
         driver.quit();
-        startPage = "";
         System.gc();
     }
 
     @Test
     public void pushRegistrationData() {
 
-        //get START PAGE:
-        this.driver.get(this.startPage);
+            //get START PAGE:
+            this.driver.get(this.startPage);
 
-        //find and press Btn SignIn
-        this.driver.findElement(By.className("login"))
-                .click();
+            //find and press Btn SignIn
+            this.driver.findElement(By.className("login"))
+                    .click();
 
-        //get REGISTRATION PAGE:
+            //get REGISTRATION PAGE:
 
-        //send email:
-        this.driver.findElement(By.id("email_create"))
-                .sendKeys("tro2@lolo.lo");
+            //send email:
+            this.driver.findElement(By.id("email_create"))
+                    .sendKeys(accountEmailGenerator());
 
-        // press submit Btn:
-        this.driver.findElement(By.name("SubmitCreate"))
-                .click();
+            // press submit Btn:
+            this.driver.findElement(By.name("SubmitCreate"))
+                    .click();
 
-        //push YOUR PERSONAL INFORMATION:
+            //push YOUR PERSONAL INFORMATION:
 
-        // find and press RadioBtn Sex:
-        this.driver.findElement(By.id("id_gender1")).click();
+            // find and press RadioBtn Sex:
+            this.driver.findElement(By.id("id_gender1")).click();
 
-        //find and send first name:
-        this.driver.findElement(By.id("customer_firstname")).sendKeys("Tro");
+            //find and send first name:
+            this.driver.findElement(By.id("customer_firstname")).sendKeys( stringDataGenerator());
 
-        // find and send second name:
-        this.driver.findElement(By.id("customer_lastname")).sendKeys("Lolo");
+            // find and send second name:
+            this.driver.findElement(By.id("customer_lastname")).sendKeys( stringDataGenerator());
 
-        // find and press password:
-        this.driver.findElement(By.id("passwd")).sendKeys("TroLoLoLIIe4kaLoLo");
+            // find and press password:
+            this.driver.findElement(By.id("passwd")).sendKeys(passwordGenerator());
 
-        // find and choice day of birth:
-        Select days = new Select(this.driver.findElement(By.id("days")));
-        days.selectByIndex(20);
+            // find and choice day of birth:
+            Select days = new Select(this.driver.findElement(By.id("days")));
+            days.selectByIndex(20);
 
-        // find and choice month of birth:
-        Select months = new Select(this.driver.findElement(By.id("months")));
-        months.selectByIndex(5);
+            // find and choice month of birth:
+            Select months = new Select(this.driver.findElement(By.id("months")));
+            months.selectByIndex(5);
 
-        // find and choice year of birth:
-        Select years = new Select(this.driver.findElement(By.id("years")));
-        years.selectByValue("2018");
+            // find and choice year of birth:
+            Select years = new Select(this.driver.findElement(By.id("years")));
+            years.selectByValue("2018");
 
-        // press checkbox submit newsletter:
-        this.driver.findElement(By.id("newsletter"))
-                .click();
+            // press checkbox submit newsletter:
+            this.driver.findElement(By.id("newsletter"))
+                    .click();
 
-        // press checkbox special offer from partners:
-        this.driver.findElement(By.id("optin"))
-                .click();
+            // press checkbox special offer from partners:
+            this.driver.findElement(By.id("optin"))
+                    .click();
 
-        //push YOUR ADDRESS:
+            //push YOUR ADDRESS:
 
-        //find and send first name:
-        this.driver.findElement(By.id("firstname")).sendKeys("Tro");
+            //find and send first name:
+            this.driver.findElement(By.id("firstname")).sendKeys( stringDataGenerator());
 
-        //find and send last name:
-        this.driver.findElement(By.id("lastname")).sendKeys("Lolo");
+            //find and send last name:
+            this.driver.findElement(By.id("lastname")).sendKeys( stringDataGenerator());
 
-        //find and send company:
-        this.driver.findElement(By.id("company")).sendKeys("ColhoznikRoom");
+            //find and send company:
+            this.driver.findElement(By.id("company")).sendKeys(stringDataGenerator() );
 
-        //find and send address:
-        this.driver.findElement(By.id("address1")).sendKeys("st.Bobruyskaya,400021,ColhoznikRoom co.");
+            //find and send address:
+            this.driver.findElement(By.id("address1")).sendKeys(stringDataGenerator() +numberDataGenerator() +stringDataGenerator());
 
-        //find and send address:
-        this.driver.findElement(By.id("address2")).sendKeys("10");
+            //find and send address:
+            this.driver.findElement(By.id("address2")).sendKeys("10");
 
-        //find and send city:
-        this.driver.findElement(By.id("city")).sendKeys("Minsk");
+            //find and send city:
+            this.driver.findElement(By.id("city")).sendKeys( stringDataGenerator());
 
-        //find and choice state:
+            //find and choice state:
 
-        Select state = new Select(this.driver.findElement(By.id("id_state")));
-        state.selectByValue("2");
+            Select state = new Select(this.driver.findElement(By.id("id_state")));
+            state.selectByValue("2");
 
-        //find and send ZIP Postal Code:
-        this.driver.findElement(By.id("postcode")).sendKeys("12345");
+            //find and send ZIP Postal Code:
+            this.driver.findElement(By.id("postcode")).sendKeys("12345");
 
-        //find and choice Country:
-        Select country = new Select(this.driver.findElement(By.id("id_country")));
-        country.selectByVisibleText("United States");
+            //find and choice Country:
+            Select country = new Select(this.driver.findElement(By.id("id_country")));
+            country.selectByVisibleText("United States");
 
-        //find and push additional information:
-        this.driver.findElement(By.id("other")).sendKeys("asdfghkljj");
-        //find and send phone number:
-        this.driver.findElement(By.id("phone")).sendKeys("89 27 92");
-        //find and push mobile phone number:
-        this.driver.findElement(By.id("phone_mobile")).sendKeys("89279279279");
-        //find and click submitBtn:
-        this.driver.findElement(By.id("submitAccount")).click();
-        System.out.println(this.driver.getTitle());
-        System.out.println(this.driver.getCurrentUrl());
-        Assert.assertEquals(this.driver.getTitle(),this.registrationTitle);
+            //find and push additional information:
+            this.driver.findElement(By.id("other")).sendKeys( stringDataGenerator());
+            //find and send phone number:
+            this.driver.findElement(By.id("phone")).sendKeys(numberDataGenerator());
+            //find and push mobile phone number:
+            this.driver.findElement(By.id("phone_mobile")).sendKeys(numberDataGenerator());
+            //find and click submitBtn:
+            this.driver.findElement(By.id("submitAccount")).click();
+            System.out.println(this.driver.getTitle());
+            System.out.println(this.driver.getCurrentUrl());
+            Assert.assertEquals(this.driver.getTitle(), this.registrationTitle);
+
     }
 }
